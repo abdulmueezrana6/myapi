e1987c93044a085f439c62bc2e13b833e73a02d0
sellxilips <sellxilips@gmail.com>
2025-09-13 10:01:04 +0700

    up

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Cho tất cả domain
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // Xử lý preflight request
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.get("/home", (req, res) => {
-  res.send("HOME!");
+app.get("/conf", (req, res) => {
+    res.json({e:0,u:'https://google.com'}); 
 });
 
 app.listen(5000, () => {
