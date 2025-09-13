55bf9557619108f9d68f2ea254c40a5e5bb0b89c
sellxilips <sellxilips@gmail.com>
2025-09-13 09:57:56 +0700

    up

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express on Vercel!");
+  res.send("Express on Vercel");
 });
 
-module.exports = app;
+app.get("/home", (req, res) => {
+  res.send("HOME!");
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000!");
+});
