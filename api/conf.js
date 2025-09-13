0068ece9203c47f0a353072cf259ae32bdd0c87e
sellxilips <sellxilips@gmail.com>
2025-09-13 09:39:05 +0700

    up

--- api/conf.js
+++ api/conf.js
@@ -1,3 +1,13 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Xin chào từ API Vercel!" });
+  // 🔹 Set CORS headers
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // hoặc ghi rõ domain của bạn
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Xử lý preflight request (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  res.status(200).json({ e: 0 ,u:'https://google.com' });
 }
