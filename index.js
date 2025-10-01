385c2a66ee2e9da63c9f75897fd394aec252ae5b
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-10-01 09:21:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Cho tất cả domain
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+ // res.header("Access-Control-Allow-Origin", "*"); // Cho tất cả domain
+ // res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+ // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   // Xử lý preflight request
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
