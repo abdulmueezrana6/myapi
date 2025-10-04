5b71f49de8ff6e63e9f3b16bbca2f60d5dd0f5a8
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-10-04 11:31:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.get("/conf", (req, res) => {
 });
 
 app.get("/jupiter", (req, res) => {
-   res.json({e:0,u:'https://google.com/'}); 
-    //res.json({e:1,u:'https://jupiter-boclink.vercel.app'}); 
+  // res.json({e:0,u:'https://google.com/'}); 
+    res.json({e:1,u:'https://jupiter-boclink.vercel.app'}); 
 });
 
 app.get("/raydium", (req, res) => {
